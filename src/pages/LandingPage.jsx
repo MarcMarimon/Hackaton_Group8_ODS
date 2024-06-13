51330f1d9ddf8eb8a9e00365f7f9d4d6a7116380
Lulirodri98 <106028055+Lulirodri98@users.noreply.github.com>
2024-06-13 13:09:35 +0200

    Merge Luchi branch

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,14 +2,6 @@
 
 const LandingPage = () => {
   return (
-<<<<<<< HEAD
-    <div className="bg-[url('./app/images/BackgroundImage.jpg')] w-[100vw] h-[100vh] bg-cover">
-        <div className="h-[100%] w-[100%]">
-            <h1>Bienvenido a EduLink!</h1>
-            <h2>Tu educación, a tu alcance.</h2>
-        </div>
-    </div>
-=======
     <>
     <div className="bg-[url('./app/images/BackgroundImage.jpg')] w-[100vw] h-[100vh] bg-cover opacity-20 fixed"></div>
     <div className="h-[100vh] flex flex-col justify-center items-center text-white">
@@ -17,7 +9,6 @@ const LandingPage = () => {
         <h2 className="font-semibold text-5xl">Tu educación, a tu alcance.</h2>
     </div>
     </>
->>>>>>> Luchi
   )
 }
 
