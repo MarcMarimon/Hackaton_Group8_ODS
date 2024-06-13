db987797d521c3fe69f6e0e0b934d38ec6fa825e
Lulirodri98 <106028055+Lulirodri98@users.noreply.github.com>
2024-06-13 13:06:36 +0200

    LandingPage finalizado

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,12 +2,13 @@
 
 const LandingPage = () => {
   return (
-    <div className="bg-[url('./app/images/BackgroundImage.jpg')] w-[100vw] h-[100vh] bg-cover">
-        <div className="m-auto">
-            <h1>Bienvenido a EduLink!</h1>
-            <h2>Tu educación, a tu alcance.</h2>
-        </div>
+    <>
+    <div className="bg-[url('./app/images/BackgroundImage.jpg')] w-[100vw] h-[100vh] bg-cover opacity-20 fixed"></div>
+    <div className="h-[100vh] flex flex-col justify-center items-center text-white">
+        <h1 className="font-bold text-7xl">Bienvenido a EduLink!</h1>
+        <h2 className="font-semibold text-5xl">Tu educación, a tu alcance.</h2>
     </div>
+    </>
   )
 }
 
