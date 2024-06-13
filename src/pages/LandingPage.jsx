dc9cbe8c13ca9cbd44eac1ae98f5506d81c6d892
Maxi <maaxiliria@gmail.com>
2024-06-13 14:31:44 +0200

    maxi

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,9 @@
 
-
 const LandingPage = () => {
   return (
     <>
     <div className="bg-[url('./app/images/BackgroundImage.jpg')] w-[100vw] h-[100vh] bg-cover opacity-20 fixed"></div>
-    <div className="h-[100vh] flex flex-col justify-center items-center text-white">
+    <div className="h-[90vh] flex flex-col justify-center items-center text-white">
         <h1 className="font-bold text-7xl">Bienvenido a EduLink!</h1>
         <h2 className="font-semibold text-5xl">Tu educación, a tu alcance.</h2>
     </div>
