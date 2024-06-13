11cff7773fac5cbaa603a9df7723d9aa897a884b
Maxi <maaxiliria@gmail.com>
2024-06-13 16:59:56 +0200

    maxi

--- src/components/Cursos.jsx
+++ src/components/Cursos.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import { obtenerCursos } from "../api/cursosApi.js";
-import PopUp from "./PopUp.jsx";
+
+import Curso from "./Curso.jsx";
 
 const Cursos = () => {
   const [cursos, setCursos] = useState([]);
-  const [active, setAvtive] = useState(false);
+
 
   useEffect(() => {
     obtenerCursos().then((data) => {setCursos(data)});
@@ -16,20 +17,7 @@ const Cursos = () => {
       </h1>
       <div className="flex gap-5 flex-wrap items-center justify-center text-center mt-10 w-[80%] m-auto">
         {cursos.map(curso => 
-            <>
-            <section key={curso.id} className="border w-[200px] h-[200px] p-5 shadow-lg hover:border-2 flex flex-col justify-between">
-                <div>
-                    <h3>{curso.titulo}</h3>
-                    <p>{curso.descripcion}</p>
-                    <p>{curso.profesor}</p>
-                    <p>{curso.fecha}</p>
-                </div>
-                <div>
-                    <button className="text-[#017C9B]" onClick={() => setAvtive(!active)}>Ver +</button>
-                </div>
-            </section>
-            {active && <PopUp curso={curso}/>}
-            </>
+          <Curso key={curso.id} curso={curso}/>
         )}
       </div>
     </>
