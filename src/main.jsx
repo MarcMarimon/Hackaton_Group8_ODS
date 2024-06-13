8bfa1f7969d6f6ef1b69e1d62044e158ce9ec30a
Maxi <maaxiliria@gmail.com>
2024-06-13 15:29:20 +0200

    maxi

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import Layout from './pages/Layout.jsx'
 import LandingPage from './pages/LandingPage.jsx'
 import Login from './pages/Login.jsx'
 import Register from './pages/Register.jsx'
-import Cursos from './components/Cursos.jsx'
+import Home from './pages/Home.jsx'
 import UserProvider from './app/UserProvider.jsx'
 
 
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<LandingPage />}></Route>
-          <Route path='/cursos' element={<Cursos />}></Route>
+          <Route path='/home' element={<Home />}></Route>
         </Route>
         <Route path='/login' element={<Login />}></Route>
         <Route path='/register' element={<Register />}></Route>
