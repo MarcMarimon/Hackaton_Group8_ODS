2b15857a56c8ccf4e2407896ae775a766388e40a
Marc Marimon <crammarimon@hotmail.com>
2024-06-13 13:08:56 +0200

    ApiProvider, cursos y categorias

--- src/api/categoriasApi.js
+++ src/api/categoriasApi.js
@@ -1,9 +1,14 @@
 import { supabase } from './supabase'
 
+let categorias = []
+
 export const obtenerCategorias = async () => {
-    const { data, error } = await supabase.from('categorias').select('*')
-    if (error) {
-        throw new Error(error.message)
+    if (categorias.length === 0) {
+        const { data, error } = await supabase.from('categorias').select('*')
+        if (error) {
+            throw new Error(error.message)
+        }
+        categorias = data
     }
-    return data
+    return categorias
 }
